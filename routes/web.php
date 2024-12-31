6d02ce354459f253609ea5ecb44405ce772308c4
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-31 23:02:23 +0800

    0825練習2-6

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,14 @@ use App\Models\Post;
 
 Route::get('/', function () {
     //return view('welcome');
-    $post = new Post();
-    $post->title='test title';
-    $post->content = 'test content';
-    $post->save();
+    //$post = new Post();
+    //$post->title='test title';
+    //$post->content = 'test content';
+    //$post->save();
+
+    Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+
+    ]);
 });
