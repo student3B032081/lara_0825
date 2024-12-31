724b5f6ffb2e53ab7e5d036347564f1380224413
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-31 22:16:20 +0800

    comments資料表

--- database/migrations/2024_12_31_141151_create_comments_table.php
+++ database/migrations/2024_12_31_141151_create_comments_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('content');
+            $table->bigInteger('post_id');
         });
     }
 
